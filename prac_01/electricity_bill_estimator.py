04e151c68b236dd970f98fd680e0be145f31f53a
chloeharrison18 <chloe.harrison@my.jcu.edu.au>
2018-08-14 13:54:32 +1000

    Updated electricity_bill_estimator file. Not completed.

---
+++ prac_01/electricity_bill_estimator.py
@@ -0,0 +1,8 @@
+def main():
+    print("Electricity bill estimator")
+    cents_per_kwh = int(input("Enter cents per kWh: "))
+    kwh_daily_use = float(input("Enter daily use in kWh: "))
+    billing_days = int(input("Enter number of billing days: "))
+
+
+main()
